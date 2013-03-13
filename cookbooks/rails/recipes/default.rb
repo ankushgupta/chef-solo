576c8c930d06ea6a3b4096d7e82cec8d7799a84e
ankushgupta <ankush@jombay.com>
2013-03-13 14:17:13 +0530

    Update default.rb

--- cookbooks/rails/recipes/default.rb
+++ cookbooks/rails/recipes/default.rb
@@ -1,7 +1,1 @@
-if node[:rails][:db_type] == "mysql"
-  require_recipe "rails::mysql"
-elsif node[:rails][:db_type] == "postgresql"
-  require_recipe "rails::postgresql"
-else
-  raise "A valid db_type should be selected"
-end
+gem_package "rails"
